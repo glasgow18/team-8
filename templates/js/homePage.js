d3f594f74864555e75f0897ddf559a601a4f980f
Ronny <zz35@st-andrews.ac.uk>
2018-10-27 22:40:14 +0100

    #JScoming

--- templates/js/homePage.js
+++ templates/js/homePage.js
@@ -5,3 +5,11 @@ function shiftOn() {
 function shiftOut() {
     document.getElementById("start").style.background='#FFA500';
 }
+
+function changeOn() {
+    document.getElementById("escapeButton").style.background='#FFD700';
+}
+
+function changeOut() {
+    document.getElementById("escapeButton").style.background='#FFFF00';
+}
