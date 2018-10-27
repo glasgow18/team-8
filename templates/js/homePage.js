983f40909fad7efae4f6ed9855cafb5119912219
maisie_mcgregor@outlook.com <Bubblesort16>
2018-10-27 22:53:18 +0100

    #javascriptaintdoingthething

--- templates/js/homePage.js
+++ templates/js/homePage.js
@@ -6,16 +6,15 @@ function shiftOut() {
     document.getElementById("start").style.background='#FFA500';
 }
 
-<<<<<<< HEAD
 function changeOn() {
     document.getElementById("escapeButton").style.background='#FFD700';
 }
 
 function changeOut() {
     document.getElementById("escapeButton").style.background='#FFFF00';
-=======
+}
+
 function closewindow()
 {
 window.close()
->>>>>>> b92e474e913c45f75838f1156dd85accfce548a4
 }
