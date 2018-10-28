438bc04ba8245b57f770abb1891eb413379d825d
Cristina <cristina.bianca1609@gmail.com>
2018-10-28 05:41:39 +0000

    population script #2
    updated population script
    changed the eachOrganisation function
    defined populate function

--- codeforgood/population_script.py
+++ codeforgood/population_script.py
@@ -1,19 +1,31 @@
 import os
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'codeforgood.settings')
 import django
+
 django.setup()
 
 from health_in_mind.models import organisations
 
 
-def eachOrganisation(name, description, websiteUrl, emailAddress, phoneNumber,
-	min_age, max_age, contact_name, gender, location, key_words):
+def eachOrganisation(name, description, websiteUrl, emailAddress, phoneNumber, min_age, max_age, contact_name, gender,
+                     location, key_words):
+    organisation = organisations.objects.get_or_create(name=name, description=description,
+                                                       websiteUrl=websiteUrl, emailAddress=emailAddress,
+                                                       phoneNumber=phoneNumber, max_age=max_age,
+                                                       contact_name=contact_name, location=location,
+                                                       key_words=key_words)
+    organisation.save()
+    return organisation
 
-	rowan = organisations.objects.get_or_create('rowan' ,'The ROWAN project encourages people to enjoy the benefits of being close to nature and the sense of wellbeing it offers.' , 'http://www.health-in-mind.org.uk/services/rowan_respect_our_woodland_and_nature/d16/#parentHorizontalTab1' , 'phil.morris@orchardcentreservices.org.uk', '01316631616', 16, 100, 0, 'midlothian', 'exercise, art, environment')
+
+def populate():
+    rowan = eachOrganisation('rowan',
+                             'The ROWAN project encourages people to enjoy the benefits of being close to nature and the sense of wellbeing it offers.',
+                             'http://www.hesalth-in-mind.org.uk/services/rowan_respect_our_woodland_and_nature/d16/#parentHorizontalTab1',
+                             'phil.morris@orchardcentreservices.org.uk',
+                             '01316631616', '16', '100', 'Phil Morris', '0', 'midlothian', 'exercise, art, environment')
 
 
-
-
-
-
+populate()
