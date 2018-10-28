0b6321c9bdf023ecca516070ee75b0019ed3cbee
Cristina <cristina.bianca1609@gmail.com>
2018-10-28 03:49:15 +0000

    commit all

--- codeforgood/codeforgood/settings.py
+++ codeforgood/codeforgood/settings.py
@@ -14,8 +14,12 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-print(BASE_DIR)
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+print(STATIC_DIR)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -70,7 +74,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'codeforgood.wsgi.application'
 
-
+STATICFILES_DIRS = [STATIC_DIR, ]
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
